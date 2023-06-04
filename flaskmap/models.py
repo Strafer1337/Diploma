528eb3cce3a3dfed6b2d9c78562e36e526995ad4
Strafer1337 <vova.ilin2000@yandex.ru>
2023-06-04 10:51:17 +0300

    70%

--- flaskmap/models.py
+++ flaskmap/models.py
@@ -5,7 +5,18 @@ class Marker(db.Model):
     latitude = db.Column(db.Float, nullable=False)
     longtitude = db.Column(db.Float, nullable=False)
     content = db.Column(db.String(200), nullable=False)
-    color = db.Column(db.String(20), nullable=False, default='red')
+    color = db.Column(db.String(10), nullable=False, default='red')
+    upvotes = db.Columd(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return f"Marker('{self.id}', '{self.latitude}', '{self.longtitude}')"
+    
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(30), nullable=False)
+    content = db.Column(db.String(200), nullable=False)
+    marker_id = db.Column(db.Integer, db.ForeignKey('marker.id'), nullable=False)
+    upvotes = db.Columd(db.Integer, nullable=True, default=0)
+
+    def __repr__(self):
+        return f"Comment('{self.id}'. {self.author}: '{self.content}; Upvotes: {self.upvotes}')"
--- flaskmap/routes.py
+++ flaskmap/routes.py
@@ -3,33 +3,34 @@ from flaskmap import app, db
 from flaskmap.forms import AddForm
 from flaskmap.models import Marker 
 
-markers = [
-    {
-        'id': 1,
-        'latitude': 55.70045,
-        'longtitude': 37.87769,
-        'content': 'Москва, ул. Камова, д. 28',
-        'color': 'red'
-    },
-    {
-        'id': 2,
-        'latitude': 55.88104,
-        'longtitude': 37.68363,
-        'content': 'Москва, ул. Тайнинская, д. 9',
-        'color': 'green'
-    },
-    {
-        'id': 3,
-        'latitude': 55.751999,
-        'longtitude': 37.617734,
-        'content': 'Московский кремль',
-        'color': 'orange'
-    }
-]
+# markers = [
+#     {
+#         'id': 1,
+#         'latitude': 55.70045,
+#         'longtitude': 37.87769,
+#         'content': 'Москва, ул. Камова, д. 28',
+#         'color': 'red'
+#     },
+#     {
+#         'id': 2,
+#         'latitude': 55.88104,
+#         'longtitude': 37.68363,
+#         'content': 'Москва, ул. Тайнинская, д. 9',
+#         'color': 'green'
+#     },
+#     {
+#         'id': 3,
+#         'latitude': 55.751999,
+#         'longtitude': 37.617734,
+#         'content': 'Московский кремль',
+#         'color': 'orange'
+#     }
+# ]
 
 @app.route("/")
 @app.route("/home")
 def home():
+    markers = Marker.query.all()
     return render_template('home.html', markers=markers, title='Home')
 
 @app.route("/about")
@@ -38,6 +39,7 @@ def about():
 
 @app.route("/map")
 def map():
+    markers = Marker.query.all()
     return render_template('map.html', markers=markers)
 
 @app.route("/add", methods=['GET', 'POST'])
